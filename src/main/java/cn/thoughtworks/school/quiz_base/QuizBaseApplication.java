e6b4da4995d047f838a60b0e71c31f0fcbffa1ea
Shunfa Xu <gouwa5700@gmail.com>
2018-02-28 16:06:30 +0800

    base quiz master

---
+++ src/main/java/cn/thoughtworks/school/quiz_base/QuizBaseApplication.java
@@ -0,0 +1,12 @@
+package cn.thoughtworks.school.quiz_base;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizBaseApplication.class, args);
+	}
+}
